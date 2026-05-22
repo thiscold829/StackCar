e67d7370da031e27dda15f1e9674119d26c76843
thiscold829 <107271781+thiscold829@users.noreply.github.com>
2026-05-22 18:27:46 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+const CONFIG = {
+  SHEET_ID: '1AozZaL30w7mLQGoXmtouRI6Ppgb7s7sDfgNTG8zaeyQ',
+  API_URL: 'https://script.google.com/macros/s/AKfycbxP4yKScy1rKkaahTtIamYyjnG0Wzi-W6EMp_KiB7j6wxmXiEwMQSNvT2yz1aw3I3v7Ow/exec'
+};
